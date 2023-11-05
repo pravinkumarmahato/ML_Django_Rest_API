845267076fc0d21808fb66cfc8149cd7cf04371b
Pravin Kumar Mahato <praveenmahato026@gmail.com>
2023-11-05 23:47:29 +0530

    commit

--- djangoApi/api/prediction/predictionModel.py
+++ djangoApi/api/prediction/predictionModel.py
@@ -1,8 +1,9 @@
 import pickle
 
+
 def predictLoanType(predictionData):
 
-    loaded_model = pickle.load(open('C:\\Users\\lenovo\\OneDrive\\lenovo g3 backup\\Desktop\\Kaiburr\\djangoApi\\api\\prediction\\models\\consumer_complaint_model', 'rb'))
-    fitted_Vectorizer = pickle.load(open('C:\\Users\\lenovo\\OneDrive\\lenovo g3 backup\\Desktop\\Kaiburr\\djangoApi\\api\\prediction\\models\\fittedVectorizer', 'rb'))
+    loaded_model = pickle.load(open('C:\\Users\\lenovo\\OneDrive\\lenovo g3 backup\\Desktop\\Kaiburr\\djangoApi\\api\\prediction\\mlModels\\consumer_complaint_model', 'rb'))
+    fitted_Vectorizer = pickle.load(open('C:\\Users\\lenovo\\OneDrive\\lenovo g3 backup\\Desktop\\Kaiburr\\djangoApi\\api\\prediction\\mlModels\\fittedVectorizer', 'rb'))
     pred = loaded_model.predict(fitted_Vectorizer.transform([predictionData]))
     return pred
